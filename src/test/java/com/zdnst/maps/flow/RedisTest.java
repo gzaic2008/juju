d46418eecafefc5f89245a1ace6d2e57dbf91adc
timpeng <timpeng@tom.com>
2015-04-15 15:05:14 +0800

    add: redis tool

--- src/test/java/com/zdnst/maps/flow/RedisTest.java
+++ src/test/java/com/zdnst/maps/flow/RedisTest.java
@@ -28,7 +28,7 @@ public class RedisTest extends AbstractTestCase {
 		// 连接redis服务器，192.168.0.100:6379
 		jedis = new Jedis("192.168.1.50", 6379);
 		// 权限认证
-		//jedis.auth("admin");
+		jedis.auth("password");
 	}
 
 	/**
